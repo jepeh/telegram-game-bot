2eb3f60fbce0e59bd8bf1a21bf78b3c5c304856b
jepeh <ganzanjepeh01@gmail.com>
2024-06-25 14:29:55 +0800

    jasper game

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,40 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
+
   const [count, setCount] = useState(0)
+  const [scale, setScale] = useState(1)
+
+  const clickMe = ()=>{
+    setCount(count + 1)
+   setScale(1.2)
+
+   setTimeout(()=>{
+    setScale(1)
+   }, 200)
+  
+  }
+
+const colors = ["#FFEB", "#2E7D32", "#64B5F6", "#FFAB91"]
+
+  const generateRandomColor = () => {
+    // Generating a random hexadecimal color
+    var ran = Math.floor(Math.random() * 6);
+    const randomColor = colors[ran]
+    
+    return randomColor;
+  };
+
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    
+    <img className='relative w-[230px] select-none draggable-none' src="src\assets\jasper.png"/>
+
+    <p onClick={clickMe} className={`bg-white p-3 text-4xl text-[#000] rounded-[10px] select-none scale-[${scale}]`}>Click Me!</p>
+    <p className='text-5xl mt-10'>{count}</p> 
+
     </>
   )
 }
